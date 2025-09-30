702502e9511332f0e7bcf3b3c73fff43268ac1e0
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-09-30 19:43:00 +0500

    Task2

---
+++ Tyuiu.MakarovAY.Sprint1.Task2.V27/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.MakarovAY.Sprint1.Task2.V27.Lib;
+namespace Tyuiu.MakarovAY.Sprint1.Task2.V27
+{
+    class Programm()
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Макаров А. М. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые Навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Макаров А. М.   | РППб-25-1                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данныые,*");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение x");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Периметр квадрата = " + ds.CalculateSquarePerimetr(x));
+
+            Console.ReadKey();
+        }
+    }
+}
