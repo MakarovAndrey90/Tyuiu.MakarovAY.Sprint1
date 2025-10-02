9b951e517db31f81c2f74737699c2509f2c8e56d
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-02 14:11:47 +0500

    Task6

---
+++ Tyuiu.MakarovAY.Sprint1.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MakarovAY.Sprint1.Task6.V13.Lib;
+namespace Tyuiu.MakarovAY.Sprint1.Task6.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            string strTest = "abcd";
+            DataService ds = new DataService();
+            Assert.AreEqual(true, ds.CheckWordsAlphabet(strTest));
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint1.Task6.V13/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.MakarovAY.Sprint1.Task6.V13.Lib;
+namespace Tyuiu.MakarovAY.Sprint1.Task6.V13
+{
+    class Programm()
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Макаров А. М. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые Навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Макаров А. М.   | РППб-25-1                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, что буквы     *");
+            Console.WriteLine("* строки упорядочены по алфавиту                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите  строку");
+            string strTest = Console.ReadLine();
+            bool res = ds.CheckWordsAlphabet(strTest);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
