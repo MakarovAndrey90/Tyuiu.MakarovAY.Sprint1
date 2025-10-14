b6c620c768e3e543233b9f34481ea69293c0a4d8
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-14 14:11:47 +0500

    Task3

--- Tyuiu.MakarovAY.Sprint1.Task3.V18/Program.cs
+++ Tyuiu.MakarovAY.Sprint1.Task3.V18/Program.cs
@@ -6,13 +6,13 @@ namespace Tyuiu.MakarovAY.Sprint1.Task2.V27
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            Console.Title = "Спринт #1 | Выполнил: Макаров А. М. | РППб-25-1";
+            Console.Title = "Спринт #1 | Выполнил: Макаров А. Я. | РППб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Базовые Навыки работы в C#                                        *");
             Console.WriteLine("* Задание #3                                                              *");
             Console.WriteLine("* Вариант #18                                                             *");
-            Console.WriteLine("* Выполнил: Макаров А. М.   | РППб-25-1                                   *");
+            Console.WriteLine("* Выполнил: Макаров А. Я.   | РППб-25-1                                   *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу, которая вычисляет, сколько квадратов со стороной C  *");
