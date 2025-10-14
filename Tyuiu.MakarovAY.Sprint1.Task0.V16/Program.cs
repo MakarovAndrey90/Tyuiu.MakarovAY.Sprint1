402a42795374dae443412965ff34cf9542e4ec01
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-14 14:10:29 +0500

    Task0

--- Tyuiu.MakarovAY.Sprint1.Task0.V16/Program.cs
+++ Tyuiu.MakarovAY.Sprint1.Task0.V16/Program.cs
@@ -7,13 +7,13 @@ namespace Tyuiu.MakarovAY.Sprint1.Task0.V16
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            Console.Title = "Спринт #1 | Выполнил: Макаров А. М. | РППб-25-1";
+            Console.Title = "Спринт #1 | Выполнил: Макаров А. Я. | РППб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Базовые Навыки работы в C#                                        *");
             Console.WriteLine("* Задание #0                                                              *");
             Console.WriteLine("* Вариант #16                                                             *");
-            Console.WriteLine("* Выполнил: Макаров А. М.   | РППб-25-1                                   *");
+            Console.WriteLine("* Выполнил: Макаров А. Я.   | РППб-25-1                                   *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу, которая вычисляет выражение 6/2/3+1-6/2*3           *");
