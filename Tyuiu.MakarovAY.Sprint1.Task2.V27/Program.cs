66e4d49cf3b03eab538b25959aaf871ea05a170c
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-14 14:11:26 +0500

    Task2

--- Tyuiu.MakarovAY.Sprint1.Task2.V27/Program.cs
+++ Tyuiu.MakarovAY.Sprint1.Task2.V27/Program.cs
@@ -6,13 +6,13 @@ namespace Tyuiu.MakarovAY.Sprint1.Task2.V27
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            Console.Title = "Спринт #2 | Выполнил: Макаров А. М. | РППб-25-1";
+            Console.Title = "Спринт #2 | Выполнил: Макаров А. Я. | РППб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #2                                                               *");
             Console.WriteLine("* Тема: Базовые Навыки работы в C#                                        *");
             Console.WriteLine("* Задание #2                                                              *");
             Console.WriteLine("* Вариант #27                                                             *");
-            Console.WriteLine("* Выполнил: Макаров А. М.   | РППб-25-1                                   *");
+            Console.WriteLine("* Выполнил: Макаров А. Я.   | РППб-25-1                                   *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данныые,*");
